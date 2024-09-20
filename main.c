b839698841e29c7b5dd42bd44d86402323f354d0
vel22788 <158492077+vel22788@users.noreply.github.com>
2024-09-20 12:03:08 -0600

    Main del STM32
    Main del STM32

--- main.c
+++ main.c
@@ -1,62 +1,299 @@
-#define F_CPU 16000000
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include "UART/UART.h"
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
-void _init_() {
-	
-	// Configuración de pines como entrada
-	DDRC &= ~((1 << DDC0) | (1 << DDC1)); // PC0, PC1 como entrada
-	DDRB &= ~((1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3)); // PB0, PB1, PB2, PB3 como entrada
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+/* USER CODE END Includes */
 
-	// Habilitar resistencias pull-up internas
-	PORTC |= (1 << DDC0) | (1 << DDC1); // Habilitar pull-ups en PC0 y PC1
-	PORTB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3); // Habilitar pull-ups en PB0, PB1, PB2 y PB3
-	
-	// Configuración de los pines PC0, PC1 (PCINT8, PCINT9)
-	PCICR |= (1 << PCIE1); // Habilitar interrupciones por cambio de pin para PCINT[14:8] (Port C)
-	PCMSK1 |= (1 << PCINT8) | (1 << PCINT9); // Habilitar PCINT8 (PC0) y PCINT9 (PC1)
-	
-	// Configuración de los pines PB0, PB1, PB2, PB3 (PCINT0, PCINT1, PCINT2, PCINT3)
-	PCICR |= (1 << PCIE0); // Habilitar interrupciones por cambio de pin para PCINT[7:0] (Port B)
-	PCMSK0 |= (1 << PCINT0) | (1 << PCINT1) | (1 << PCINT2) | (1 << PCINT3); // Habilitar PB0, PB1, PB2, PB3
-	
-	// Habilitar interrupciones globales
-	sei();
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+uint8_t buffer[1];
+uint8_t mensaje[10];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_IT(&huart1, buffer, 1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
-// ISR para los pines PB0, PB1, PB2, PB3 (PCINT0, PCINT1, PCINT2, PCINT3)
-ISR(PCINT0_vect) {
-	_delay_ms(20);
-	if (!(PINB & (1 << PINB0))) {
-		writeUART('3');
-	}
-	if (!(PINB & (1 << PINB1))) {
-		writeUART('4');
-	}
-	if (!(PINB & (1 << PINB2))) {
-		writeUART('5');
-	}
-	if (!(PINB & (1 << PINB3))) {
-		writeUART('6');
-	}
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-// ISR para los pines PC0 y PC1 (PCINT8, PCINT9)
-ISR(PCINT1_vect) {
-	_delay_ms(20);
-	if (!(PINC & (1 << PINC0))) {
-		writeUART('1');
-	}
-	if (!(PINC & (1 << PINC1))) {
-		writeUART('2');
-	}
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
-int main(void) {
-	_init_();
-	initUART9600();	
-	while (1) {
-	}
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 9600;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
 }
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+
+  if (buffer[0]=='1'){
+	  strcpy((char*)mensaje, "Arriba");
+	  HAL_UART_Transmit(&huart2, mensaje, sizeof(mensaje), 1000);
+  }
+  if (buffer[0]=='2'){
+  	  strcpy((char*)mensaje, "Abajo");
+  	  HAL_UART_Transmit(&huart2, mensaje, sizeof(mensaje), 1000);
+    }
+  if (buffer[0]=='3'){
+  	  strcpy((char*)mensaje, "Derecha");
+  	  HAL_UART_Transmit(&huart2, mensaje, sizeof(mensaje), 1000);
+    }
+  if (buffer[0]=='4'){
+  	  strcpy((char*)mensaje, "Izquierda");
+  	  HAL_UART_Transmit(&huart2, mensaje, sizeof(mensaje), 1000);
+    }
+  if (buffer[0]=='5'){
+  	  strcpy((char*)mensaje, "A");
+  	  HAL_UART_Transmit(&huart2, mensaje, sizeof(mensaje), 1000);
+    }
+  if (buffer[0]=='6'){
+  	  strcpy((char*)mensaje, "B");
+  	  HAL_UART_Transmit(&huart2, mensaje, sizeof(mensaje), 1000);
+    }
+  memset(mensaje, 0, sizeof(mensaje));
+  //HAL_UART_Transmit(&huart2, buffer, sizeof(buffer), 1000);
+  HAL_UART_Receive_IT(&huart1, buffer, 1);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
